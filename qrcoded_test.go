1d65de6092e8082ed2dd6a664d00dcfd4fcf9295
Marcelo Aguero <marceloaguero@gmail.com>
2020-03-18 17:18:56 -0300

    Refactor test

--- qrcoded_test.go
+++ qrcoded_test.go
@@ -6,21 +6,14 @@ import (
 	"testing"
 )
 
-func TestGenerateQRCodeReturnsValue(t *testing.T) {
-	result := GenerateQRCode("555-2368")
-
-	if result == nil {
-		t.Errorf("Generated QRCode is nil")
-	}
-
-	if len(result) == 0 {
-		t.Errorf("Generated QRCode has no data")
-	}
-}
-
 func TestGenerateQRCodeGeneratesPNG(t *testing.T) {
-	result := GenerateQRCode("555-2369")
-	buffer := bytes.NewBuffer(result)
+	buffer := new(bytes.Buffer)
+	GenerateQRCode(buffer, "555-2369")
+
+	if buffer.Len() == 0 {
+		t.Errorf("No QRCode generated")
+	}
+
 	_, err := png.Decode(buffer)
 	if err != nil {
 		t.Errorf("Generated QR Code is not a png: %s", err)
